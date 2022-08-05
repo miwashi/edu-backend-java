77d17775dbd8ebc70249315b44aade8415539b64
Mikael Wallin <mikael.x.wallin@gmail.com>
2022-08-05 20:48:57 +0200

    Added auth methods

--- app/src/main/java/net/miwashi/edu/controller/SessionController.java
+++ app/src/main/java/net/miwashi/edu/controller/SessionController.java
@@ -3,7 +3,6 @@ package net.miwashi.edu.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import net.miwashi.edu.security.Credentials;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,32 +12,33 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/session")
-public class SessionController {
+public class SessionController {//17.0.2-oracle 11.0.14-zulu
 
     private static final Logger LOGGER = LoggerFactory.getLogger( SessionController.class );
 
-    @GetMapping(path="/auth", produces = "application/json")
-    public ResponseEntity<?> authorizeWithGet(@NotNull @RequestParam(name = "credentials") String credentialsParam) {
-        var credentials = jsonToCredentials(credentialsParam);
-        LOGGER.info(credentials.toString());
-        return ResponseEntity.ok("Returning JWT");
-    }
-
-    @GetMapping(path="/auth", produces = "application/json")
-    public ResponseEntity<?> authorizeWithGet(@NotNull @RequestParam(name = "user") String user, @NotNull @RequestParam(name = "password") String password) {
-        if(user == null){
+    @GetMapping(path = "/auth", params = {"user", "password"})
+    public ResponseEntity<?> getIt(@RequestParam String user, @RequestParam String password){
+        if(user == null || user.isBlank()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-        if(password == null){
+        if(password == null || password.isBlank()){
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
         }
-
         var credentials = new Credentials(user, password);
-
-        LOGGER.info(credentials.toString());
-        return ResponseEntity.ok("Returning JWT");
+        LOGGER.info("Attempting to authorize user: " + credentials.getUser());
+        return ResponseEntity.ok("Returning JWT for user and password");
     }
 
+    @GetMapping(path = "/auth", params = {"credentials"})
+    public ResponseEntity<?> getIt(@RequestParam(name = "credentials") String credentialsParam){
+        var credentials = jsonToCredentials(credentialsParam);
+        if(credentials==null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        LOGGER.info("Attempting to authorize user: " + credentials.getUser());
+        return ResponseEntity.ok("Returning JWT for credentials");
+    }
+    
     @PostMapping(path="/auth", produces = "application/json")
     public ResponseEntity<?> authorizeWithPost(@RequestBody Credentials credentials) {
         LOGGER.info(credentials.toString());
@@ -46,16 +46,16 @@ public class SessionController {
     }
 
     private Credentials jsonToCredentials(String credentialsJson) {
+        if(credentialsJson==null || credentialsJson.isBlank()){
+            return null;
+        }
         Credentials credentials = null;
         try {
             credentials = (new ObjectMapper()).readValue(credentialsJson, Credentials.class);
         } catch (JsonProcessingException e) {
-            LOGGER.error("Exception thrown in parsing json credentials!",e);
+            LOGGER.error("Parsing Exception thrown when parsing json credentials!",e);
         } catch (Throwable e){
-            LOGGER.error("Exception thrown in parsing json credentials!",e);
-        }
-        if(credentials==null){
-            credentials = new Credentials();
+            LOGGER.error("Unknown Exception thrown when parsing json credentials!",e);
         }
         return credentials;
     }
